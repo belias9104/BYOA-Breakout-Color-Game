ebf976653783cdef6dbbcf00d401bbe88c0c896b
Benjamin Elias <belias9104@jh-mba158-10.d214.org>
2018-05-02 14:34:15 -0500

    hello

--- Breakout Color Game/GameScene.swift
+++ Breakout Color Game/GameScene.swift
@@ -11,8 +11,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+   
     
     override func didMove(to view: SKView) {
         
