a8102330e5b82e1e1b8715549220526fe2fb1f14
Benjamin Elias <belias9104@jh-mba158-10.d214.org>
2018-05-03 14:54:02 -0500

    Starting Code

--- Breakout Color Game/GameScene.swift
+++ Breakout Color Game/GameScene.swift
@@ -9,8 +9,39 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+let ballCategory: UInt32 = 0x1 << 0
+let paddleCategory: UInt32 = 0x2 << 1
+let leftCategory: UInt32 = 0x2 << 2
+let rightCategory: UInt32 = 0x3 << 3
+let bottomCategory: UInt32 = 0x4 << 4
+let topCategory: UInt32 = 0x5 << 5
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    var topPaddle = SKSpriteNode()
+    var bottomPaddle = SKSpriteNode()
+    var rightPaddle = SKSpriteNode()
+    var leftPaddle = SKSpriteNode()
+    var ball = SKSpriteNode()
     
+    var started = false
+    var counter = 0
+    var label = SKLabelNode()
+    
+    override func didMove(to view: SKView) {
+        //contact delegate
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        //bitmasks
+    }
+    func changePaddle(_ node: SKSpriteNode) {
+        //paddle color
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //movement of paddles and delay start
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //movement of paddles
+    }
 }
     
