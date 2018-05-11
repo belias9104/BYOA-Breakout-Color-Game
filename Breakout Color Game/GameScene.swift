d1086b3de2ee277c413630f8f975e75f3c890f2d
mobileapps <mobileapps@jh-mba158-26.d214.org>
2018-05-11 14:32:40 -0500

    added some background music I made on my iPad

--- Breakout Color Game/GameScene.swift
+++ Breakout Color Game/GameScene.swift
@@ -8,7 +8,7 @@
 
 import SpriteKit
 import GameplayKit
-
+var backgroundMusic: SKAudioNode!
 let ballCategory: UInt32 = 0x1 << 0
 let paddleCategory: UInt32 = 0x2 << 1
 let borderCategory: UInt32 = 0x2 << 2
@@ -35,7 +35,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
-        
+        if let musicURL = Bundle.main.url(forResource: "My Song 68", withExtension: "m4a") {
+            backgroundMusic = SKAudioNode(url: musicURL)
+            addChild(backgroundMusic)
+        }
         label = SKLabelNode(text: String(counter))
         label.fontSize = 100.0
         label.position = CGPoint(x: 0, y: -35)
@@ -97,7 +100,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             sleep(3)
             print("check")
             if (ball.physicsBody?.velocity.dy)! <= CGFloat(50) {
-                ball.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 200))
+                ball.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 300))
+            } else if (ball.physicsBody?.velocity.dx)! <= CGFloat(50) {
+                ball.physicsBody?.applyImpulse(CGVector(dx: 300, dy: 0))
             }
         }
     }
